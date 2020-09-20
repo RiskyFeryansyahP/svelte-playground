fb27a327f1a567b741819db70b6f80edafa075d8
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2020-09-20 23:47:49 +0700

    3. Component, CSS & Conditional Styles, Props

---
+++ Component, CSS & Conditional Styles, Props/src/App.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	// importing
+	import Modal from './components/Modal.svelte'
+
+	let peoples = [
+		{ id: 1, name: 'Risky Feryansyah', age: 21, belt: 'black'},
+		{ id: 2, name: 'Shania Saraswati', age: 21, belt: 'yellow' },
+		{ id: 3, name: 'Cindy Puspita', age:18, belt: 'red' },
+	]
+
+	const num: number = 5
+	const message: string = "Hello, im a prop value"
+
+	// delete data from peoples
+	const handleClick = (e: MouseEvent, id: number) => {
+		peoples = peoples.filter((people) => people.id !== id)
+	}
+</script>
+
+<Modal message={message} isPromo={false} /> <!-- passing message and is promo props to modal component-->
+<main>
+	{#each peoples as people (people.id)}
+		<div>
+			<h4> Name: {people.name} </h4>
+			{#if people.belt === 'black'}
+				<strong> MASTER NINJA </strong>
+			{/if}
+			<p> {people.age} years old, {people.belt} belt. </p>
+			<button on:click={(e) => handleClick(e, people.id)}> Delete </button>
+		</div>
+		
+		{:else}
+			<p> There are no people to show... </p> 
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ Component, CSS & Conditional Styles, Props/src/components/Modal.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let message: string = "Default value"
+
+    const isShowModal = true
+    export let isPromo: boolean = false
+</script>
+
+{#if isShowModal}
+    <div class="backdrop" class:promo={isPromo}> <!-- class:promo is conditional style, where promo is name of class css -->
+        <div class="modal">
+            <p> {message} </p>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.8);
+    }
+
+    .modal {
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background: white;
+    }
+
+    .promo .modal {
+        background: crimson;
+        color: white;
+    }
+</style>
